e9547810c3e05cf3e78c786e4faf5de0debc8e96
Oscar Triano dotoscat <user@example.com>
2017-07-29 12:50:21 +0200

    Load assets

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
 
+import pyglet
+
+assets_list = {
+    "car": "car.png",
+    "block": "block.png",
+}
+
 if __name__ == "__main__":
-    print("Hellow!")
+    pyglet.resource.path = ["assets"]
+    pyglet.resource.reindex()
+
+    assets = {key : pyglet.resource.image(assets_list[key]) for key in assets_list}
