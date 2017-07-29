0c885fb25ac823bef0f21d7c0df6be82b896318f
Oscar Triano dotoscat <user@example.com>
2017-07-29 19:21:36 +0200

    function create

--- game/pool.py
+++ game/pool.py
@@ -3,19 +3,20 @@ import toyblock
 from system import graphics, physics, platform
 from components import Body, Platform, PlatformSprite
 
-car = toyblock.Pool(
+def create(assets):
+    global car = toyblock.Pool(
         1,
         (Body, Sprite),
         ((True,), (assets["car"],)),
         systems=(physics, graphics))
 
-platform = toyblock.Pool(
-    8,
-    (Body, Platform, PlatformSprite),
-    (None, None, (assets["block"],)),
-    systems=(physics, platform)
-)
+    global platform = toyblock.Pool(
+        8,
+        (Body, Platform, PlatformSprite),
+        (None, None, (assets["block"],)),
+        systems=(physics, platform)
+    )
 
-@platform_pool.init
-def init_platform(entity):
-    entity[Platform].size = 4
+    @platform_pool.init
+    def init_platform(entity):
+        entity[Platform].size = 4
