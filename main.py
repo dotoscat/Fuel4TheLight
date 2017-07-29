7a88cbaade3b7892ab75603a204c8db7a57a9424
Oscar Triano dotoscat <user@example.com>
2017-07-29 12:57:58 +0200

    Create Window

--- main.py
+++ main.py
@@ -8,7 +8,15 @@ assets_list = {
 }
 
 if __name__ == "__main__":
+    class GameWindow(pyglet.window.Window):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
     pyglet.resource.path = ["assets"]
     pyglet.resource.reindex()
 
     assets = {key : pyglet.resource.image(assets_list[key]) for key in assets_list}
+
+    game_window = GameWindow()
+
+    pyglet.app.run()
