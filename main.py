115b4ef5c0587580d888a43436510aec76d2fd42
Oscar Triano dotoscat <user@example.com>
2017-07-29 14:44:12 +0200

    Use toyblock

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
 
 import pyglet
+import toyblock
 
 assets_list = {
     "car": "car.png",
     "block": "block.png",
 }
+
+class Body:
+    def __init__(self):
+        self.x = 0.0
+        self.y = 0.0
+        self.vel_x = 0.0
+        self.vel_y = 0.0
+
+    def update(self, dt):
+        self.x += self.vel_x*dt
+        self.y += self.vel_y*dt
+
+@toyblock.system
+def physics(system, entity, dt):
+    entity[Body].update(dt)
 
 if __name__ == "__main__":
     class GameWindow(pyglet.window.Window):
@@ -23,5 +39,10 @@ if __name__ == "__main__":
     icon = (pyglet.image.Texture.create_for_size(assets["car"].target, 16, 16).
             get_image_data())
     game_window.set_icon(icon)
+
+    car_pool = toyblock.Pool(1, (Body,), systems=(physics,))
+    pyglet.clock.schedule(physics)
+
+    car = car_pool.get()
 
     pyglet.app.run()
