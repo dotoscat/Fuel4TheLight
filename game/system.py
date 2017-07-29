eb6b21ccc3aaa3b00268e8ac532711ca69351ba7
Oscar Triano dotoscat <user@example.com>
2017-07-29 19:01:33 +0200

    Move systems to file

---
+++ game/system.py
@@ -0,0 +1,19 @@
+from components import Body, Platform, PlatformSprite
+
+@toyblock.system
+def physics(system, entity, dt, gravity):
+    body = entity[Body]
+    body.update(dt, gravity)
+
+@toyblock.system
+def graphics(system, entity):
+    body = entity[Body]
+    entity[Sprite].set_position(body.x, body.y)
+
+@toyblock.system
+def platform(system, entity):
+    body = entity[Body]
+    platform = entity[PlatformSprite]
+    platform.x = body.x
+    platform.y = body.y
+    platform.times = entity[Platform].size
--- main.py
+++ main.py
@@ -11,24 +11,6 @@ assets_list = {
     "block": "block.png",
 }
 
-@toyblock.system
-def physics(system, entity, dt, gravity):
-    body = entity[Body]
-    body.update(dt, gravity)
-
-@toyblock.system
-def graphics(system, entity):
-    body = entity[Body]
-    entity[Sprite].set_position(body.x, body.y)
-
-@toyblock.system
-def platform(system, entity):
-    body = entity[Body]
-    platform = entity[PlatformSprite]
-    platform.x = body.x
-    platform.y = body.y
-    platform.times = entity[Platform].size
-
 if __name__ == "__main__":
     class GameWindow(pyglet.window.Window):
         VWIDTH = 210
