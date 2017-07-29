65fcd6aa7fdd420f8d29cc134e66ebf7cb83c24e
Oscar Triano dotoscat <user@example.com>
2017-07-29 20:24:01 +0200

    Move GameWindow to system

--- game/system.py
+++ game/system.py
@@ -1,6 +1,31 @@
+import pyglet
 from pyglet.sprite import Sprite
+from pyglet.gl import glViewport, glOrtho, glMatrixMode, glLoadIdentity
+from pyglet import gl
 import toyblock
 from .components import Body, Platform, PlatformSprite
+
+class GameWindow(pyglet.window.Window):
+    VWIDTH = 210
+    VHEIGHT = 160
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.sprites = []
+
+    def on_draw(self):
+        self.clear()
+        for sprite in self.sprites:
+            sprite.draw()
+
+    def add_Sprite(self, Sprite_):
+        self.sprites.append(Sprite_)
+
+    def on_resize(self, width, height):
+        glViewport(0, 0, width, height)
+        glMatrixMode(gl.GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho(0, GameWindow.VWIDTH, 0, GameWindow.VHEIGHT, -1, 1)
+        glMatrixMode(gl.GL_MODELVIEW)
 
 @toyblock.system
 def physics(system, entity, dt, gravity):
--- main.py
+++ main.py
@@ -1,35 +1,10 @@
 #!/usr/bin/env python
 
 import pyglet
-from pyglet.gl import glViewport, glOrtho, glMatrixMode, glLoadIdentity
-from pyglet import gl
 from pyglet.sprite import Sprite
-import toyblock
 import game.pool as pool
 import game.system as system
 from game.components import Body, Platform, PlatformSprite
-
-class GameWindow(pyglet.window.Window):
-        VWIDTH = 210
-        VHEIGHT = 160
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.sprites = []
-
-        def on_draw(self):
-            self.clear()
-            for sprite in self.sprites:
-                sprite.draw()
-
-        def add_Sprite(self, Sprite_):
-            self.sprites.append(Sprite_)
-
-        def on_resize(self, width, height):
-            glViewport(0, 0, width, height)
-            glMatrixMode(gl.GL_PROJECTION)
-            glLoadIdentity()
-            glOrtho(0, GameWindow.VWIDTH, 0, GameWindow.VHEIGHT, -1, 1)
-            glMatrixMode(gl.GL_MODELVIEW)
 
 assets_list = {
     "car": "car.png",
@@ -41,7 +16,7 @@ if __name__ == "__main__":
     pyglet.resource.reindex()
 
     assets = {key : pyglet.resource.image(assets_list[key]) for key in assets_list}
-    game_window = GameWindow(caption="Fuel4TheLight")
+    game_window = system.GameWindow(caption="Fuel4TheLight")
     icon = (pyglet.image.Texture.create_for_size(assets["car"].target, 16, 16).
             get_image_data())
     game_window.set_icon(icon)
