3c6adbb389f94485c34d2ae3afd14969b981803c
Oscar Triano dotoscat <user@example.com>
2017-07-29 13:18:29 +0200

    Set caption. Load icon

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
     pyglet.resource.reindex()
 
     assets = {key : pyglet.resource.image(assets_list[key]) for key in assets_list}
-
-    game_window = GameWindow()
+    game_window = GameWindow(caption="Fuel4TheLight")
+    icon = pyglet.image.Texture.create_for_size(assets["car"].target, 16, 16).get_image_data()
+    game_window.set_icon(icon)
 
     pyglet.app.run()
