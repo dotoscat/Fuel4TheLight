660b4c07587487b70b944cb3ff9833819a9c0a3b
Oscar Triano dotoscat <user@example.com>
2017-07-29 13:25:00 +0200

    Clear window. Load icon with Texture

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ if __name__ == "__main__":
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
 
+        def on_draw(self):
+            self.clear()
+
     pyglet.resource.path = ["assets"]
     pyglet.resource.reindex()
 
     assets = {key : pyglet.resource.image(assets_list[key]) for key in assets_list}
     game_window = GameWindow(caption="Fuel4TheLight")
-    icon = pyglet.image.Texture.create_for_size(assets["car"].target, 16, 16).get_image_data()
+    icon = (pyglet.image.Texture.create_for_size(assets["car"].target, 16, 16).
+            get_image_data())
     game_window.set_icon(icon)
 
     pyglet.app.run()
