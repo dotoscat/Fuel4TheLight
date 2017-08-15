9db72fc075d77198f327c9611b780762b8b47aab
Oscar Triano dotoscat <user@example.com>
2017-08-15 17:53:18 +0200

    Replace layers by groups

--- game/scene.py
+++ game/scene.py
@@ -5,20 +5,26 @@ class Scene(object):
         A scene will...
         1. Handle input,
         2. loop
-        3. what to draw -> layers
+        3. what to draw -> groups!
     """
 
-    def __init__(self, n_layers):
+    def __init__(self, n_groups):
         self._batch = pyglet.graphics.Batch()
-        self._layers = [pyglet.graphics.OrderedGroup(i) for i in range(n_layers)]
+        self._groups = [pyglet.graphics.OrderedGroup(i) for i in range(n_layers)]
 
     @property
     def batch(self):
         return self._batch
 
     @property
-    def layer(self):
-        return self._layers
+    def group(self):
+        return self._groups
 
     def draw(self):
         self._batch.draw()
+
+    def init(self):
+        pass
+
+    def quit(self):
+        pass
