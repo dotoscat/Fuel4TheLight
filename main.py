67c5e360835077c978e99a43363f8df680619d81
Oscar Triano dotoscat <user@example.com>
2017-08-19 13:17:14 +0200

    Remove unused code from main.py

--- main.py
+++ main.py
@@ -52,22 +52,10 @@ if __name__ == "__main__":
     pyglet.resource.reindex()
 
     assets = {key : pyglet.resource.image(assets_list[key]) for key in assets_list}
-    #game_window = system.GameWindow(assets, system.GameWindow.VWIDTH*2,
-    #    system.GameWindow.VHEIGHT*2, caption="Fuel4TheLight")
     #icon = (pyglet.image.Texture.create_for_size(assets["car"].target, 16, 16).
     #        get_image_data())
     #game_window.set_icon(icon)
 
-    #pool.create(assets, game_window.batch, game_window.layer)
-    #game_state = GameState(game_window)
-
-    #pool.platform.clean(game_state.free)
-    #pool.powerup.clean(game_state.free)
-    #pool.car.clean(game_state.free)
-    #pyglet.clock.schedule(game_state.loop)
-    #pyglet.clock.schedule(system.do, -GameState.GRAVITY, game_state)
-
-    #game_state.init()
     Title = Title(assets)
     engine = Engine(assets)
     director = Director(
